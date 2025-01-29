4417c90a3822f42fad6ba4525e138015ff2acd5e
michael-0007a <152276463+michael-0007a@users.noreply.github.com>
2025-01-29 18:37:05 +0530

    Add files via upload

---
+++ Assignment 1 string methods.py
@@ -0,0 +1,49 @@
+# strings methods like upper, lower soon
+# strings are immutable 
+
+
+
+a = "watermelon"
+b = "WATERMELON"
+print(a.upper()) # converts into upper string
+print(b.lower()) # converts into lower string
+
+
+# strip
+c = "  hello brother  "
+print(c.strip())
+
+
+# rstrip : which is used to remove the mentioned string
+d = "harsha !!!"
+print(d.rstrip("!")
+
+
+#replace
+print(a.replace("watermelon","harsha"))
+
+#split
+e = "hello, bro how are you"
+print(e.split())
+
+#capitalize: capitalize the each starting word
+print(e.capitalize())
+
+
+# center
+
+print(a.center(50))
+
+#count : counting the specific word how many times repetiting
+print(a.count("a"))
+
+#find
+print(a.find("a")
+
+#index:
+f = "iam fine"
+print(f.index("fine"))
+
+#title
+print(f.title())
+
